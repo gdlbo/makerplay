3df53b5909ddf9deb2037df684a02147479e59c8
Artur Skubei <41114720+gdlbo@users.noreply.github.com>
2026-09-11 19:28:55 +0300

    Bump app version to 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
 
     defaultConfig {
         applicationId = "io.github.gdlbo.makerplay"
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
     }
 
     buildTypes {
